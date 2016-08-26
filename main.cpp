c1f09a34eaaa50b0ab0762f0454d62dc4a84bf31
khuang <khuang@localhost>
2016-08-26 23:07:25 +0000

    OLED Text Example

--- main.cpp
+++ main.cpp
@@ -1,64 +1,54 @@
 #include "mbed.h"
 #include "Hexi_OLED_SSD1351.h"
 #include "OLED_types.h"
-#include "OLED_fonts.h"
+#include "OpenSans_Font.h"
 #include "string.h"
 
 int main() {
-    
-    //Instantiate Time    
-    Timer time;
+    char text[20];  /* Text Buffer */ 
+    Timer time;     /* Instantiate Time */
 
-    //Instantiate the SSD1351 OLED Driver    
-    SSD1351 oled(PTB22,PTB21,PTC13,PTB20,PTE6, PTD15); // (MOSI,SCLK,POWER,CS,RST,DC)
-    //Turn on the backlight of the OLED Display 
+    /* Instantiate the SSD1351 OLED Driver */ 
+    SSD1351 oled(PTB22,PTB21,PTC13,PTB20,PTE6, PTD15); /* (MOSI,SCLK,POWER,CS,RST,DC) */
+    
+    /* Get OLED Class Default Text Properties */
+    oled_text_properties_t textProperties = {0};
+    oled.GetTextProperties(&textProperties);    
+
+    /* Turn on the backlight of the OLED Display */
     oled.DimScreenON();
-    //Fills the screen with solid black         
+    
+    /* Fills the screen with solid black */         
     oled.FillScreen(COLOR_BLACK);
-    
-    //Set the parameters for desired text properties
-     oled_text_properties_t
-            textProperties =
-            {
-            .font       = oledFont_Tahoma_8_Regular,    
-            .fontColor  = COLOR_WHITE,
-            .alignParam = OLED_TEXT_ALIGN_LEFT, //currently does nothing in this example
-            .background = NULL
-            };
 
-    oled.SetTextProperties(&textProperties);
-
-    //Approximately 15 characters can fit on one line for this font size. 
-    //Extra element for null terminator of string. 
-    char text[15+1];
-    
-    //Display Text at (x=13,y=0). Need to manually center. 
+    /* Display Text at (x=7,y=0) */
     strcpy((char *) text,"TEXT EXAMPLE");
-    oled.DrawText((uint8_t *)text,13,0);
-    
-    //Change Font to Blue 
-    textProperties.fontColor = COLOR_BLUE;
+    oled.Label((uint8_t *)text,7,0);
+        
+    /* Change font color to blue */ 
+    textProperties.fontColor   = COLOR_BLUE;
     oled.SetTextProperties(&textProperties);
 
-    //Display text at (x=5,y=24)
+    /* Display text at (x=5,y=40) */ 
     strcpy(text,"Timer(s):");
-    oled.DrawText((uint8_t *)text,5,24);
+    oled.Label((uint8_t *)text,5,40);
         
-    //Set text color to white
+    /* Set text properties to white and right aligned for the dynamic text */
     textProperties.fontColor = COLOR_WHITE;
+    textProperties.alignParam = OLED_TEXT_ALIGN_RIGHT;
     oled.SetTextProperties(&textProperties);    
 
-    //start timer
-    time.start();
+    
+    time.start(); /* start timer */
 
     while (true) {
-
-        //Dynamic Text
         
-        //Display Time Reading at (x=50, y=24)
+        /* Format the time reading */
         sprintf(text,"%.2f",time.read());
-        oled.DrawText((uint8_t *)text,50,24);
-
+        
+        /* Display time reading in 35px by 15px textbox at(x=55, y=40) */
+        oled.TextBox((uint8_t *)text,55,40,35,15); 
+             
         Thread::wait(1000);
     }
 }
